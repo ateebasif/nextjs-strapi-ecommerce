cbbfe1b621ebd0e12df01a3d3fcd3ef62eac9092
ateebasif <ateeb.asif902@gmail.com>
2023-05-27 22:15:36 +0500

    project setup

---
+++ components/Navbar.jsx
@@ -0,0 +1,48 @@
+import Link from "next/link";
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="body-font text-gray-600">
+      <div className="container mx-auto flex flex-col flex-wrap items-center py-5 md:flex-row">
+        <Link
+          href="/"
+          className="title-font mb-4 flex items-center font-medium text-gray-900 md:mb-0"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            className="h-10 w-10 rounded-full bg-indigo-500 p-2 text-white"
+            viewBox="0 0 24 24"
+          >
+            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+          </svg>
+          <span className="ml-3 text-xl">MyShop</span>
+        </Link>
+        <nav className="flex flex-wrap items-center justify-center text-base md:ml-auto">
+          <Link href="/" className="mr-5 hover:text-gray-900">
+            Home
+          </Link>
+          <Link href="/about" className="mr-5 hover:text-gray-900">
+            About
+          </Link>
+          <Link href="/products" className="mr-5 hover:text-gray-900">
+            Products
+          </Link>
+          <Link href="/contact" className="mr-5 hover:text-gray-900">
+            Contact
+          </Link>
+        </nav>
+        <button className="my-2 rounded border-0 bg-indigo-500 px-4 py-2 text-white hover:bg-indigo-600 focus:outline-none">
+          Login
+        </button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
